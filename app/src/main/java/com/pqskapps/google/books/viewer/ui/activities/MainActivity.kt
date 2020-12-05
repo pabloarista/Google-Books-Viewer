eff548eea5ef89082a77f4e0535c0abb4235dd54
pqsk <pqsk>
2020-12-04 20:00:01 -0500

    added root activity

---
+++ app/src/main/java/com/pqskapps/google/books/viewer/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pqskapps.google.books.viewer.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pqskapps.google.books.viewer.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
