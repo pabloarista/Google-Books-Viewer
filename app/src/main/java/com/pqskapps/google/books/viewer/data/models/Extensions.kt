fb06a14b8531bbe30af0fb074e9cc2655a31423c
pqsk <pqsk>
2020-12-13 18:37:39 -0500

    Fixed http to https. This is to resolve nothing being shown on Android 9+ devices (source: https://developer.android.com/training/articles/security-config#CleartextTrafficPermitted)  "Starting with Android 9 (API level 28), cleartext support is disabled by default.". Even though google.com redirects to https, this still presents an issue, thus this fix will work for all versions

--- app/src/main/java/com/pqskapps/google/books/viewer/data/models/Extensions.kt
+++ app/src/main/java/com/pqskapps/google/books/viewer/data/models/Extensions.kt
@@ -4,21 +4,30 @@ import com.pqskapps.google.books.viewer.data.models.api.Item
 import com.pqskapps.google.books.viewer.data.models.api.Root
 import com.pqskapps.google.books.viewer.data.models.api.VolumeInfo
 
+const val HTTP: String = "http:"
 fun Root?.toBooks(): List<Book> {
     val books = mutableListOf<Book>()
     if(this != null) {
         for (item: Item in this.items.filterNotNull()) {
             val volumeInfo: VolumeInfo? = item.volumeInfo
             val book = Book(
-                volumeInfo?.imageLinks?.smallThumbnail?: ""
-                , volumeInfo?.title?: ""
-                , volumeInfo?.authors?: listOf<String>()
-                , volumeInfo?.publishedDate?: "Unknown"
-                , volumeInfo?.description?: ""
-                , item.accessInfo?.webReaderLink?: ""
-                , item.accessInfo?.accessViewStatus != "NONE")
+                    volumeInfo?.imageLinks?.smallThumbnail?.toHttps()?: ""
+                    , volumeInfo?.title?: ""
+                    , volumeInfo?.authors?: listOf<String>()
+                    , volumeInfo?.publishedDate?: "Unknown"
+                    , volumeInfo?.description?: ""
+                    , item.accessInfo?.webReaderLink?.toHttps()?: ""
+                    , item.accessInfo?.accessViewStatus != "NONE")
             books.add(book)
         }//for
     }//if
     return books
+}
+fun String?.toHttps(): String? {
+    var str: String? = this
+
+    if(!str.isNullOrBlank() && str.startsWith(HTTP, true)) {
+        str = "https" + str.substring(str.indexOf(HTTP) + HTTP.length - 1)
+    }//if
+    return str
 }
